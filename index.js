4c2a5928bbf8a585eafa83e09533e88b9a621fe2
pricbnll <pricbnll@gmail.com>
2024-06-21 21:46:03 -0300

    feat: localStorage

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
+function carregarInteresses() {
+    const listaInteresses  = document.querySelector("#listaInteresses")
+    
+    listaInteresses.HTML = "";
+    
+    const interesses = JSON.parse(localStorage.getItem("meus-interesses") || [])
+    interesses.forEach(interesse => {
+        const novoInteresse = document.createElement("li");
+        novoInteresse.innerHTML = interesse;
+        listaInteresses.appendChild(novoInteresse)
+    })
+}
 
-const adicionarInteresse = document.querySelector("input")
-const listaInteresses  = document.querySelector("ul")
-
-const botaoAdicionar = document.querySelector(".button-add")
-
-botaoAdicionar.addEventListener("click", () => {
-    const value = adicionarInteresse.value
-    if (value === "") {
+function adicionarInteresse () {
+    const adicionarInteresse = document.querySelector("input")
+    const interesse= adicionarInteresse.value
+    if (interesse === "") {
         alert("Por favor insira um interesse ou hobbie")
         return;
     }
+    const interesses = JSON.parse(localStorage.getItem('meus-interesses')) || [];
+    interesses.push(interesse);
+    localStorage.setItem('meus-interesses', JSON.stringify(interesses));
 
-    const novoInteresse = document.createElement("li");
-    novoInteresse.textContent = value;
+    const novoInteresse = document.createElement('li');
+    novoInteresse.textContent = interesse;
 
+    const listaInteresses = document.getElementById('lista-interesses');
     listaInteresses.appendChild(novoInteresse);
 
-    adicionarInteresse.value = "";
-})
+    input.value = "";
+}
+
+function limparInteresses() {
+    localStorage.removeItem('meus-interesses');
+    carregarInteresses();
+}
 
 
+document.querySelector(".button-add").addEventListener('click', adicionarInteresse)
+document.querySelector('.button-clear').addEventListener('click', limparInteresses);
 
+setInterval(carregarInteresses, 1000)
 
-
-
-
-
-/* <div>
-    <strong>Lista:</strong>
-    <button class="button-clear">Limpar lista</button>
-</div> */
